c3ab03b36fd06d3c0d9a7cc22476e2e439eda7e1
xxchong <2106997706@qq.com>
2024-12-22 22:30:38 +0800

    修改了新的常用中文，加入标点符号；添加将字库读取到psram中，提高刷新速度

--- main/src/Fonts/flash_font_16_t.c
+++ main/src/Fonts/flash_font_16_t.c
@@ -11,6 +11,7 @@
 
 #include "lvgl/lvgl.h"
 #include "esp_partition.h"
+#include "esp_heap_caps.h"
 
 
 typedef struct{
@@ -33,41 +34,87 @@ typedef struct{
 
 
 static x_header_t __g_xbf_hd = {
-    .min = 0x0020,
-    .max = 0x9fa5,
+    .min = 0x0009,
+    .max = 0xffe5,
     .bpp = 4,
 };
 
 
-
-static uint8_t __g_font_buf[300];//如bin文件存在SPI FLASH可使用此buff
+static uint8_t __g_font_buf[714];//如bin文件存在SPI FLASH可使用此buff
 
 esp_partition_t* partition_res=NULL;
+// static uint8_t *__user_font_getdata(int offset, int size)
+// {
+//     static uint8_t first_in = 1;  
+//     if(first_in==1)
+//     {      
+//         partition_res=esp_partition_find_first(0x01,0x40,NULL);//这个函数第一个参数是我们分区表的第四行的，第二列的参数，第二个是第三列的值
+//         first_in=0;
+//         if (partition_res == NULL)
+//         {
+//             printf("Failed to open file for reading\n");
+//             return NULL;
+//         }
+//         else
+//         {
+//              printf("Successfully open file for reading\n");
+//         }
+//     }
+//     esp_err_t res=esp_partition_read(partition_res,offset,__g_font_buf,size);//读取数据
+//     if(res!=ESP_OK)
+//     {
+//         printf("Failed to reading\n");
+//     }
+
+//     return __g_font_buf;
+// }
+
+// 添加PSRAM缓存指针
+static uint8_t *font_psram_cache = NULL;
+static size_t font_cache_size = 0;
+
+// 修改数据获取函数
 static uint8_t *__user_font_getdata(int offset, int size)
 {
     static uint8_t first_in = 1;  
-    if(first_in==1)
+    if(first_in == 1)
     {      
-        partition_res=esp_partition_find_first(0x01,0x40,NULL);//这个函数第一个参数是我们分区表的第四行的，第二列的参数，第二个是第三列的值
-        first_in=0;
-        if (partition_res == NULL)
-        {
+        partition_res = esp_partition_find_first(0x01, 0x40, NULL);
+        if (partition_res == NULL) {
             printf("Failed to open file for reading\n");
             return NULL;
         }
-        else
-        {
-             printf("Successfully open file for reading\n");
+
+        // 获取分区大小并分配PSRAM
+        size_t partition_size = partition_res->size;
+        font_psram_cache = heap_caps_malloc(partition_size, MALLOC_CAP_SPIRAM);
+        if (font_psram_cache == NULL) {
+            printf("Failed to allocate PSRAM\n");
+            return NULL;
         }
-    }
-    esp_err_t res=esp_partition_read(partition_res,offset,__g_font_buf,size);//读取数据
-    if(res!=ESP_OK)
-    {
-        printf("Failed to reading\n");
+        font_cache_size = partition_size;
+
+        // 一次性将整个字体文件读入PSRAM
+        esp_err_t res = esp_partition_read(partition_res, 0, font_psram_cache, partition_size);
+        if (res != ESP_OK) {
+            printf("Failed to read font data to PSRAM\n");
+            heap_caps_free(font_psram_cache);
+            font_psram_cache = NULL;
+            return NULL;
+        }
+        
+        first_in = 0;
+        printf("Successfully loaded font to PSRAM\n");
     }
 
-    return __g_font_buf;
+    // 直接从PSRAM返回数据
+    if (font_psram_cache && offset + size <= font_cache_size) {
+        return &font_psram_cache[offset];
+    }
+    
+    return NULL;
 }
+
 
 
 static const uint8_t * __user_font_get_bitmap(const lv_font_t * font, uint32_t unicode_letter) {
@@ -105,13 +152,13 @@ static bool __user_font_get_glyph_dsc(const lv_font_t * font, lv_font_glyph_dsc_
 }
 
 
-//MiSans Normal,,-1
-//字模高度：21
+//Alibaba PuHuiTi 3.0 45 Light,,-1
+//字模高度：22
 //XBF字体,外部bin文件
 lv_font_t flash_font_16_t = {
     .get_glyph_bitmap = __user_font_get_bitmap,
     .get_glyph_dsc = __user_font_get_glyph_dsc,
-    .line_height = 21,
+    .line_height = 22,
     .base_line = 0,
 };
 
